99e3a380f882df965e1b83b2d68124ae7d37b1e7
Paula Berthaud <polaberto@protonmail.com>
2023-05-10 10:02:25 +0200

    Update 03_ft_print_numbers.c

--- C/C00/03_ft_print_numbers.c
+++ C/C00/03_ft_print_numbers.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <unistd.h>
+#include <unistd.h>
 
 void	ft_print_numbers(void)
 {
